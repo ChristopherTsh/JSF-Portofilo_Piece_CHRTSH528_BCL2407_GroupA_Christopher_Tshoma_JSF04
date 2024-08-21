d4a582151ae20d2c12790db579c6328d5c41cb2c
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-08-21 17:03:45 +0200

    "Updated cardRatings prop to use product.rating instead of hardcoded value"

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -134,7 +134,7 @@
             :dark-card="false"
             :card-image="product.image"
             :card-title="product.title"
-            ::cardRatings="4.5" 
+            :cardRatings="product.rating" 
             :card-description="product.description"
             :card-price="product.price"
             :card-category="product.category"
